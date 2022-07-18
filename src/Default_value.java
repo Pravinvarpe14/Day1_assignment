52da9c48e348fd418f85424dc69acd55d784e7fa
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-18 21:53:27 +0530

    Display_default_value

---
+++ src/Default_value.java
@@ -0,0 +1,25 @@
+
+public class Default_value {
+	
+	   static int a;
+	   static double b;
+	   static float c;
+	   static boolean d;
+	   static long e;
+	   static short f;
+	
+	public static void main(String[] args) {
+		
+		    System.out.println("Default value");
+	    	System.out.println("int = " + a);
+		    System.out.println("double = " + b);
+	        System.out.println("float = " + c);
+	        System.out.println("boolean = " + d);
+	        System.out.println("long = " + e);
+	        System.out.println("short = " + f);
+		
+		   
+
+	}
+
+}
