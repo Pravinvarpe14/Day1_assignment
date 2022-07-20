aa04aa7fda8d398f1ef79092e7f2a70ef56084a5
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-20 17:59:05 +0530

    sum of command line

---
+++ src/Sum_of_command_line.java
@@ -0,0 +1,15 @@
+
+public class Sum_of_command_line {
+
+	public static void main(String[] args) {
+		
+		int a,b,sum;
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		sum = a + b;
+		
+	System.out.println("addition of "+a+" "+b+" is : "+sum);
+		}
+	}
+
+
